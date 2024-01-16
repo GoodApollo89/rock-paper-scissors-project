9807a5768c02603478632a1ded8be10a2500215f
GoodApollo89 <140934111+GoodApollo89@users.noreply.github.com>
2024-01-17 10:53:23 +1100

    Add game function
    This function plays a single round of the game

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -2,10 +2,64 @@
 // if the integer produced is 1, 2 or 3. 
 function getComputerChoice(){
     if(Math.floor(Math.random() * 3) + 1 === 1){
-        return 'Rock';
+         'Rock';
     }else if(Math.floor(Math.random() * 3) + 1 === 2){
-        return 'Paper';
+         'Paper';
     }else{
-        return 'Scissors';
+         'Scissors';
     }
+    }
+
+    function playRound(playerSelection, computerSelection){
+        playerSelection = prompt('player?');
+        computerSelection = prompt('computer?');
+        if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
+            return alert('You Win! Rock beats scissors');
+        }else if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
+            return alert('You Lose! Paper beats Rock');
+        }else if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
+            return alert('Draw! No one wins');
+        }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
+            return alert('You Lose! Rock beats Scissors');
+        }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
+            return alert('You Win! Scissors beats Paper');
+        }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
+            return alert('Draw! No one wins');
+        }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
+            return alert('You Win. Paper beats Rock');
+        }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
+            return alert('You Lose. Scissors beats Paper');
+        }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
+            return alert('Draw! No one wins');
+        }
+    }
+
+    playerSelection = prompt('player?');
+    computerSelection = prompt('computer?');
+    if(playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock'  && computerSelection === 'scissors' || playerSelection === 'SCISSORS' || playerSelection === 'Scissors'){
+        return alert('You Win! Rock beats scissors');
+    }else if(playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS' && computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock'){
+        return alert('You Lose! Rock beats Scissors');
+    }
+
+    playerSelection = prompt('player?');
+    computerSelection = prompt('computer?');
+    if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
+        alert('You Win! Rock beats scissors');
+    }else if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
+        alert('You Lose! Paper beats Rock');
+    }else if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
+        alert('Draw! No one wins');
+    }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
+        alert('You Lose! Rock beats Scissors');
+    }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
+        alert('You Win! Scissors beats Paper');
+    }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
+        alert('Draw! No one wins');
+    }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
+        alert('You Win. Paper beats Rock');
+    }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
+        alert('You Lose. Scissors beats Paper');
+    }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
+        alert('Draw! No one wins');
     }
