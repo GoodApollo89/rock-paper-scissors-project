849fad38ea8a670c9098754c2f3b0608fde55a7b
GoodApollo89 <140934111+GoodApollo89@users.noreply.github.com>
2024-01-22 18:33:00 +1100

    Add scoreboard ability
    Added global variables playerScore & computerScore to playRound function
    to record win per round. Added above variables to game function to tally
    up final score of 5 rounds.

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -10,33 +10,47 @@ function getComputerChoice(){
     }
     }
 
+
+    let playerScore;
+    let computerScore;    
+
 // this function plays a single round of the game  
     function playRound(playerSelection, computerSelection){
         if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
-            return alert('You Win! Rock beats scissors');
+            alert('You Win! Rock beats Scissors'); 
+            ++playerScore;
         }else if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
-            return alert('You Lose! Paper beats Rock');
+            alert('You Lose! Paper beats Rock'); 
+            ++computerScore;
         }else if((playerSelection === 'rock' || playerSelection === 'ROCK' || playerSelection === 'Rock') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
-            return alert('Draw! No one wins');
+            alert('Draw! No one wins');
         }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
-            return alert('You Lose! Rock beats Scissors');
+            alert('You Lose! Rock beats Scissors'); 
+            ++computerScore;
         }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
-            return alert('You Win! Scissors beats Paper');
+            alert('You Win! Scissors beats Paper');
+            ++playerScore;
         }else if((playerSelection === 'scissors' || playerSelection === 'Scissors' || playerSelection === 'SCISSORS') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
-            return alert('Draw! No one wins');
+            alert('Draw! No one wins');
         }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'rock' || computerSelection === 'ROCK' || computerSelection === 'Rock')){
-            return alert('You Win. Paper beats Rock');
+            alert('You Win. Paper beats Rock');
+            ++playerScore;
         }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'scissors' || computerSelection === 'Scissors' || computerSelection === 'SCISSORS')){
-            return alert('You Lose. Scissors beats Paper');
+            alert('You Lose. Scissors beats Paper');
+            ++computerScore;
         }else if((playerSelection === 'paper' || playerSelection === 'Paper' || playerSelection === 'PAPER') && (computerSelection === 'paper' || computerSelection === 'Paper' || computerSelection === 'PAPER')){
-            return alert('Draw! No one wins');
+            alert('Draw! No one wins');
         }
     }
 
 function game(){
+    alert('This game of Rock, Paper, Scissors is best of 5 rounds.');
+    playerScore = 0;
+    computerScore = 0;
     for(let i = 0; i < 5; i++){
         playerSelection = prompt('Please choose Rock, Paper or Scissors');
         computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
     }
+    console.log('Your score is ' + playerScore + ' while the computer\'s score' + computerScore);
 }
